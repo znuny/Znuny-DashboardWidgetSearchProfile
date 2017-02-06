06cc65204cebda13b56af2c5bd89ebc91ce6575b
Thorsten Eckel <te@znuny.com>
2017-02-06 10:49:28 +0100

    WIP: Current state of development.

---
+++ Kernel/Language/de_Znuny4OTRSDashboardWidgetSearchProfile.pm
@@ -0,0 +1,26 @@
+# --
+# Kernel/Language/de_Znuny4OTRSDashboardWidgetSearchProfile.pm - the german translation of the texts of Znuny4OTRSDashboardWidgetSearchProfile
+# Copyright (C) 2012-2016 Znuny GmbH, http://znuny.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::de_Znuny4OTRSDashboardWidgetSearchProfile;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    $Self->{Translation}->{'Search Profiles'} = 'Suchvorlagen';
+    $Self->{Translation}->{'Available search profiles of the current agent'} = 'Verfügbare Suchvorlagen des momentanen Agenten';
+
+    return 1;
+}
+
+1;
