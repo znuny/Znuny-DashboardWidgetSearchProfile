e2ee758e6944fc78a664c7dd92efef27c8c8c286
Thorsten Eckel <te@znuny.com>
2017-08-17 11:34:25 +0200

    Tidied up.

--- Kernel/Output/HTML/Dashboard/TicketSearchProfile.pm
+++ Kernel/Output/HTML/Dashboard/TicketSearchProfile.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2016 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
 # --
 # $origin: https://github.com/OTRS/otrs/blob/c7771de920c53a313eadda0a954c7bc7a8a48477/Kernel/Output/HTML/Dashboard/TicketGeneric.pm
 # --
